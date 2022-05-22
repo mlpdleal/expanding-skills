147a04ec7b830b8379a8f9d3d79a2f518c82c65f
mlpdleal <manoellealdeveloper@gmail.com>
2022-05-22 15:15:00 -0300

    Completed Day 27 of 100 Days of SwiftUI

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -5,49 +5,85 @@
 //  Created by Manoel Leal on 21/05/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
-        
-        VStack{
-            Stepper("\(sleepAmount.formatted()) hours",
-                    value: $sleepAmount,
-                    in: 4...12,
-                    step: 0.25)
+        NavigationView{
+            Form{
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
                 
-            DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .date)
-            DatePicker("Please enter a hour", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            DatePicker("Please enter a date (except past date)", selection: $wakeUp, in: Date.now...)
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Desire amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    
+                }
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Daily coffe intake")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar{
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK"){}
+            } message: {
+                Text(alertMessage)
+            }
+        }
+     
+    }
+    
+    func calculateBedtime(){
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
             
-            Text(Date.now, format: .dateTime.day().month().year())
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
             
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
         
-    }
-    
-    func exempleDate(){
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
-    }
-    
-    func trivialExemple(){
-        var components = DateComponents()
-        components.hour = 8
-        components.minute = 0
-        let date = Calendar.current.date(from: components) ?? Date.now
-    }
-    
-    func trivialExemple1(){
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+       showingAlert = true
     }
 }
 
